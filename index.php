67a4178469e90137096d26b2ec158a295b266859
RoxareX <ticermark@gmail.com>
2023-10-15 20:47:03 +0300

    .

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ $method = strtolower($_SERVER["REQUEST_METHOD"]);
 
 switch($route) {
   case "/":
-    require_once __DIR__ . "/../index.html";
+    require_once __DIR__ . "/index.html";
   break;
 
   case "/calculate":
-    require_once __DIR__ . "/../calculate.html";
+    require_once __DIR__ . "/calculate.html";
   break;
 
   default:
